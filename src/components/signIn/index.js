12eade5110d141283477d9e351e328f268b9d06e
kfir.z <kfir.z@walkme.com>
2019-10-22 11:18:26 +0300

    first commit

---
+++ src/components/signIn/index.js
@@ -0,0 +1,45 @@
+
+import React, { Component } from 'react';
+import { View, Button, TextInput, Text, ActivityIndicator, StyleSheet } from "react-native";
+
+class SignIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            isLoading: false
+        };
+    }
+
+    handleOnSignIn = event => {
+        this.setState({ isLoading: !this.state.isLoading })
+    }
+
+    render() {
+        const isLoading = this.state.isLoading ? <ActivityIndicator size="large" color="#01579B" style={{ height: 50 }} /> : null;
+
+        return (
+            <View style={styles.container}>
+                <Text h1 style={styles.text}>Sign In</Text>
+                <TextInput placeholder="Email address..." style={styles.textInput}
+                    onChangeText={(email) => this.setState({ email })} />
+                <TextInput secureTextEntry placeholder="Password..." style={styles.textInput}
+                    onChangeText={(password) => this.setState({ password })} />
+
+                <Button color="#01579B" title="SIGN IN" onPress={this.handleOnSignIn} />
+
+                {isLoading}
+            </View>
+        )
+    }
+};
+
+const styles = StyleSheet.create({
+    textInput: { color: "#ffffff" },
+    container: { backgroundColor: "#E1E2E1", height: 350 },
+    text: { color: "#ffffff", fontSize: 32, backgroundColor: "#01579b", paddingTop: 20, paddingBottom: 20, textAlign: "center" }
+});
+
+
+export default SignIn;
---
+++ src/views/signIn/index.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+import SignIn from '../../components/signIn';
+
+class SignInView extends Component {
+
+    render() {
+        return (
+            <SignIn />
+        )
+    }
+};
+
+export default SignInView;
